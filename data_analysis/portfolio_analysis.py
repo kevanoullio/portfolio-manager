a46d0a6ed735067b52c243e3fc84f452f9fb7988
Kevin Ulliac <kevanoullio@outlook.com>
2023-11-22 10:59:56 -0700

    Added possibly good libraries to use for analysis.

---
+++ data_analysis/portfolio_analysis.py
@@ -0,0 +1,2 @@
+# Use https://pypi.org/project/pyfolio/ to analyze the performance and risk of the strategy
+# Also https://pypi.org/project/empyrical/ for performance analysis???
---
+++ trading_strategies/trading_strategies.py
@@ -0,0 +1,2 @@
+# Use https://pypi.org/project/TA-Lib/ for technical analysis and indicators
+# Also https://pypi.org/project/QuantLib/ for quantitative finance
