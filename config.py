3b949b272ad8062960ff891a0538bf7d5c539207
Kevin Ulliac <kevanoullio@outlook.com>
2024-12-23 22:23:42 -0700

    Fixed bug to create log directory if it doesn't exist.

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 # Description: Configuration file for the application
 
 # Standard Libraries
+import os
 import logging
 
 # Third-party Libraries
@@ -16,6 +17,13 @@ LOGGING_DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
 LOGGING_FILENAME = "./logs/portfolio_manager.log"
 
 def configure_logging():
+    # Check that the log directory exists
+    log_directory = "/".join(LOGGING_FILENAME.split("/")[:-1])
+    if not os.path.exists(log_directory):
+        print("Creating log directory: {}".format(log_directory))
+        os.makedirs(log_directory)
+    
+    # Configure the logging
     logging.basicConfig(
         level=logging.DEBUG if DEBUG_MODE else LOGGING_LEVEL,
         format=LOGGING_FORMAT,
